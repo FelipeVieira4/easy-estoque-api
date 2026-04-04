3283218014130373d62a8539e7bf665b795dbf1c
FelipeVieira4 <felipevieira58238@gmail.com>
2026-04-04 16:40:18 -0300

    fix: removed useless import

--- src/main/java/com/sysAppEstudo/easyEstoque/controller/NaturezaController.java
+++ src/main/java/com/sysAppEstudo/easyEstoque/controller/NaturezaController.java
@@ -1,7 +1,5 @@
 package com.sysAppEstudo.easyEstoque.controller;
 
-import java.net.http.HttpClient;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
