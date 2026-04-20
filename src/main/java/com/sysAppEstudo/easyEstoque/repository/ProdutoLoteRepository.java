8eaee4458a2c86955c16073aeee95a58c44c89ac
FelipeVieira4 <felipevieira58238@gmail.com>
2026-04-19 21:35:41 -0300

    feat: produto e lote para controle de estoque

---
+++ src/main/java/com/sysAppEstudo/easyEstoque/repository/ProdutoLoteRepository.java
@@ -0,0 +1,9 @@
+package com.sysAppEstudo.easyEstoque.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sysAppEstudo.easyEstoque.models.ProdutoLoteModel;
+
+@Repository
+public interface ProdutoLoteRepository  extends JpaRepository<ProdutoLoteModel,Long>{}
